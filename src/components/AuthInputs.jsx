8ba354930bd873e60a7ffa5b7740ae6f3305b742
mirabella <mirabella@192.168.1.7>
2024-06-22 02:24:15 +0700

    finishing project styling react

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 import Button from "./Button.jsx";
 import Input from "./Input.jsx";
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -31,35 +23,31 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
-          invalid={emailNotValid}
           label="Email"
+          invalid={emailNotValid}
           type="email"
-          style={{
-            backgroundColor: emailNotValid ? "#fed2d2" : "d1d5db",
-          }}
-          className={emailNotValid ? "invalid" : undefined}
           onChange={(event) => handleInputChange("email", event.target.value)}
         />
         <Input
           invalid={passwordNotValid}
           label="Password"
           type="password"
-          className={passwordNotValid ? "invalid" : undefined}
           onChange={(event) =>
             handleInputChange("password", event.target.value)
           }
         />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
